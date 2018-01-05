d5c30b49720e9c47e7123c53c01200ae7d4c5385
JAKE <JAR948@STUDENT.HKS.HARVARD.EDU>
2018-01-05 16:34:40 -0500

    add comments to sayhello

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+#this makes the website say hello to whatever name is inputed
+#depends on stdin
+#another comment
 import sys
 name = sys.stdin.read()
 print "Hello " + name + "!"
