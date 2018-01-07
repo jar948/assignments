8aa01186e4d7594cc92e7ea11f3f46ab1172f1f2
JAKE <JAR948@STUDENT.HKS.HARVARD.EDU>
2018-01-07 13:42:36 -0500

    adding in bonus function square, cube and square_n_times

--- calculator.py
+++ calculator.py
@@ -10,8 +10,27 @@ def subtract(a,b):
 def divide(a,b):
 		return a / b
 		
+def square(a):
+		return a ** 2
 
+def cube(a):
+		return a ** 3
+
+def square_n_times(a,b):
+		return a ** b
 
 print "I'm going to use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
+print x
+
+print "I'm going to use the calculator functions to square 5"
+x = square(5)
+print x
+
+print "I'm going to use the calculator functions to cube 5"
+x = cube(5)
+print x
+
+print "I'm going to use the calculator functions to square 5 five times"
+x = square_n_times(5,5)
 print x
